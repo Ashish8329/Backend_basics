4d1381098a639ae4a495d5420676cad922e93fab
Ashish auti <ashishauti123@gmail.com>
2025-11-25 09:59:32 +0000

    feat : apiview apis

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import call_api, call_html, ApiView
+from .views import call_api, call_html, ApiViews, API_withView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
 
 urlpatterns = [
     path('', call_api),
-    path('http', call_html),
-    path('view', ApiView.as_view())
+    path('http/', call_html),
+    path('view/', ApiViews.as_view()),
+    path('apiview/',API_withView.as_view() )
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -3,6 +3,9 @@ from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
 def call_api(request):
@@ -12,11 +15,13 @@ def call_html(request):
     return HttpResponse('hi')
 
 @method_decorator(csrf_exempt, name='dispatch')
-class ApiView(View):
+class ApiViews(View):
+
     def get(self, request):
         return HttpResponse('woring view')
     
     def post(self, request):
+
         import json
         body_decode = request.body.decode('utf-8')
 
@@ -26,3 +31,10 @@ class ApiView(View):
         except json.JSONDecodeError:
             body = {"error":"error"}
         return JsonResponse({"data":body})
+    
+class API_withView(APIView):
+    def get(self, request):
+        return Response(status=200, data="done")
+
+    def post(self, request):
+        return Response(status=202, data='created')
